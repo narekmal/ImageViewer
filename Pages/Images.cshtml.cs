76243d77b4a05f3ccfb2d38d35a980ca5c14be71
Narek <narek.mal@gmail.com>
2022-02-26 18:38:43 +0400

    Added folders viewer and images viewer pages

---
+++ Pages/Images.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImageViewer.Pages;
+
+public class ImagesModel : PageModel
+{
+    private readonly ILogger<ImagesModel> _logger;
+
+    public ImagesModel(ILogger<ImagesModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
