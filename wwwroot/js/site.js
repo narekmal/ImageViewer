1ad3fa0dde4870902b7cc074303a9b0d473bce8e
Narek <narek.mal@gmail.com>
2022-02-27 17:32:49 +0400

    Image select functionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", () => { 
+    const images = document.querySelectorAll(".js-image");
+
+    const handleImageClick = e => {
+        const selectedImage = document.querySelector(".js-selected-image");
+        const imgUrl = e.target.getAttribute('data-href');
+        const downloadLink = document.querySelector(".js-download");
+
+        selectedImage.setAttribute('src', imgUrl);
+
+        downloadLink.style.display = "inline";
+        downloadLink.setAttribute("href", imgUrl);
+        document.querySelector(".js-initial-state").style.display = "inline";
+        // const clickedCard = e.target.closest(".js-card")
+        // cards.forEach(card => {
+        //     card.isSameNode(clickedCard) ? card.classList.remove("js-invert-colors") : card.classList.add("js-invert-colors");
+        // });
+    }
+
+    images.forEach(card => card.addEventListener("click", handleImageClick));
+});
