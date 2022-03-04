a0dd8fb51a65dc997472cf7e941cff46bc81c8bf
Narek <narek.mal@gmail.com>
2022-03-04 12:56:35 +0400

    Added image endpoints

---
+++ api/Controllers/MainController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class MainController : ControllerBase
+{
+    private readonly ILogger<MainController> _logger;
+
+    public MainController(ILogger<MainController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "Folders")]
+    public string[] Folders()
+    {
+        string rootPath = Directory.GetCurrentDirectory() + "\\root-folder";
+        var folders = Directory.GetDirectories(rootPath);
+        var blackList = System.IO.File.ReadAllLines(rootPath + "\\black-list.txt");
+
+        string foldersLimitStr = HttpContext.Request.Query["folders_limit"];
+        foldersLimitStr = (String.IsNullOrEmpty(foldersLimitStr) ? "10" : foldersLimitStr);
+        int foldersLimit = int.Parse(foldersLimitStr);
+        foldersLimit = Math.Max(1, foldersLimit);
+
+        folders = folders.Select(path => {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                return dirInfo.Name; 
+            })
+            .Where(folderName => !blackList.Contains(folderName))
+            .Take(foldersLimit)
+            .ToArray();
+
+        return folders;
+    }
+
+    [HttpGet(Name = "Images")]
+    public Tuple<string, int, int>[] Images()
+    {
+        string folder = HttpContext.Request.Query["folder"].ToString();
+
+        string wwwRootPath = Directory.GetCurrentDirectory();
+        var files = System.IO.Directory.GetFiles(wwwRootPath + "\\root-folder\\" + folder);
+
+        var tuples = files.Select(path => {
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(path);
+            try {
+                var img = System.Drawing.Bitmap.FromFile(path);
+                return new Tuple<string, int, int>(fileInfo.Name, img.Width, img.Height);
+            }
+            catch(Exception e) {
+                return new Tuple<string, int, int>(fileInfo.Name, -1, -1);
+            }
+            }).ToArray();
+
+        return tuples;
+    }
+
+    [HttpGet]
+    public IActionResult Image()
+    {
+        string folder = HttpContext.Request.Query["folder"].ToString();
+        string imageName = HttpContext.Request.Query["image"].ToString();
+        string wwwRootPath = Directory.GetCurrentDirectory();
+        var image = System.IO.File.OpenRead(wwwRootPath + "\\root-folder\\" + folder + "\\" + imageName);
+        return File(image, "image/jpeg");
+    }
+}
